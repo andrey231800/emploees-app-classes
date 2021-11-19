4f9bd73add534636f24b25327b230c1c2fe548ac
andrey231800 <bambook231800@gmail.com>
2021-11-19 23:26:01 +0300

    class React components

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,14 @@
+import "./app-info.css";
+
+const AppInfo = ({increased, employees, rise}) => {
+    return (
+        <div className="app-info">
+            <h1>Учет сотрудников в компании N</h1>
+            <h2>Общее число сотрудников:{employees}</h2>
+            <h2>Премию получат:{increased}</h2>
+            <h2>На повышение:{rise}</h2>
+        </div>
+    )
+}
+
+export default AppInfo;
